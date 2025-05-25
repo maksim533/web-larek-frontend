005d391b7bc19956e64f05875c7e112c91a93a57
maksim <kotyukov.maks@mail.ru>
2025-05-25 13:57:43 +0300

    форматирование файла index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,38 +1,32 @@
-import { IEvents } from "../components/base/events"
-
 export interface IUser {
-  address: string
-  phone: string
-  payment: string
-  email: string
-  
+	address: string;
+	phone: string;
+	payment: string;
+	email: string;
 }
 
-
 export interface IItem {
-  id: string
-  category: string
-  title: string
-  description: string
-  image: string
-  price: number | null
+	id: string;
+	category: string;
+	title: string;
+	description: string;
+	image: string;
+	price: number | null;
 }
 
 export interface IItemView extends IItem {
-   index?:number
-   itemButton?: boolean
+	index: number;
+	itemButton: boolean;
 }
 
 export interface IOrder extends IUser {
-  total: number;
-  items: string[];
+	total: number;
+	items: string[];
 }
-
 
 export interface IOrderResult {
-  id: string;
-  total: number
+	id: string;
+	total: number;
 }
 
-export type FormErrors = Partial<Record<keyof IUser, string>>
-
+export type FormErrors = Partial<Record<keyof IUser, string>>;
